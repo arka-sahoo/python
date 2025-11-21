39f4231b8cd6866e2e231968a22ae6f960f98484
Arka Sahoo <arkasahoo2005@gmail.com>
2025-11-21 00:13:04 -0600

    "Created all the functions"

--- television.py
+++ television.py
@@ -1,2 +1,44 @@
 class Television:
+    MIN_VOLUME = 0
+    MAX_VOLUME = 2
+    MIN_CHANNEL = 0
+    MAX_CHANNEL = 0
 
+    def __init__(self):
+        self.__status = False
+        self.__muted = False
+        self.__volume = Television.MIN_VOLUME
+        self.__channel = Television.MIN_CHANNEL
+
+    def power(self):
+        self.__status = True
+
+    def mute(self):
+        self.__muted = True
+
+    def channel_up(self):
+        if self.__channel == self.MAX_CHANNEL:
+            self.__channel = self.MIN_CHANNEL
+        else:
+            self.__channel += 1
+
+    def channel_down(self):
+        if self.__channel == self.MIN_CHANNEL:
+            self.__channel = self.MAX_CHANNEL
+        else:
+            self.__channel -= 1
+
+    def volume_up(self):
+        if self.__muted:
+            self.__muted = False
+        if self.__volume < self.MAX_VOLUME:
+            self.__volume += 1
+
+    def volume_down(self):
+        if self.__muted:
+            self.__muted = False
+        if self.__volume > self.MIN_VOLUME:
+            self.__volume -= 1
+
+    def __str__(self):
+        return f'Power = {self.__status}, Channel = {self.__channel}, Volume = {self.__volume}'
