3e0869835847624f0c61ff24e23874e745515db8
Arka Sahoo <arkasahoo2005@gmail.com>
2025-11-21 14:43:11 -0600

    "Edited the Methods"

--- television.py
+++ television.py
@@ -2,43 +2,69 @@ class Television:
     MIN_VOLUME = 0
     MAX_VOLUME = 2
     MIN_CHANNEL = 0
-    MAX_CHANNEL = 0
+    MAX_CHANNEL = 3
 
     def __init__(self):
         self.__status = False
         self.__muted = False
         self.__volume = Television.MIN_VOLUME
         self.__channel = Television.MIN_CHANNEL
+        self.__previous_volume = Television.MIN_VOLUME
 
     def power(self):
-        self.__status = True
+        if not self.__status:
+            self.__status = True
+        else:
+            self.__status = False
 
     def mute(self):
-        self.__muted = True
+        if self.__status:
+            if not self.__muted:
+                self.__volume = 0
+                self.__muted = True
+            else:
+                self.__volume = self.__previous_volume
+                self.__muted = False
 
     def channel_up(self):
-        if self.__channel == self.MAX_CHANNEL:
-            self.__channel = self.MIN_CHANNEL
-        else:
-            self.__channel += 1
+        if self.__status:
+            if self.__channel == self.MAX_CHANNEL:
+                self.__channel = self.MIN_CHANNEL
+            elif self.__channel > self.MAX_CHANNEL:
+                self.__channel = self.MIN_CHANNEL
+            else:
+                self.__channel += 1
 
     def channel_down(self):
-        if self.__channel == self.MIN_CHANNEL:
-            self.__channel = self.MAX_CHANNEL
-        else:
-            self.__channel -= 1
+        if self.__status:
+            if self.__channel == self.MIN_CHANNEL:
+                self.__channel = self.MAX_CHANNEL
+            elif self.__channel < self.MIN_CHANNEL:
+                self.__channel = self.MAX_CHANNEL
+            else:
+                self.__channel -= 1
 
     def volume_up(self):
-        if self.__muted:
-            self.__muted = False
-        if self.__volume < self.MAX_VOLUME:
-            self.__volume += 1
+        if self.__status:
+            if self.__muted:
+                self.__muted = False
+                self.__volume = self.__previous_volume
+                self.__volume += 1
+
+            if self.__volume < self.MAX_VOLUME:
+                self.__volume += 1
+                self.__previous_volume = self.__volume
 
     def volume_down(self):
-        if self.__muted:
-            self.__muted = False
-        if self.__volume > self.MIN_VOLUME:
-            self.__volume -= 1
+        if self.__status:
+            if self.__muted:
+                self.__muted = False
+                self.__volume = self.__previous_volume
+                self.__volume -= 1
+                self.__previous_volume = self.__volume
+            elif self.__volume > self.MIN_VOLUME:
+                self.__volume -= 1
+                self.__previous_volume = self.__volume
 
     def __str__(self):
         return f'Power = {self.__status}, Channel = {self.__channel}, Volume = {self.__volume}'
